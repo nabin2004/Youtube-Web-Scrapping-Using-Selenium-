451d96b937dbdc1f0a910dc474e0e0d92ce8db16
nabin2004 <olinabin2004@gmail.com>
2023-06-26 13:46:49 +0545

    the messsage is not working

--- scrapper.py
+++ scrapper.py
@@ -1,26 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
+import os
 
-trending_url = 'https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
-
-response = requests.get(trending_url)
-
-print("Status Code", response.status_code)
-# print('Output', response.text[:1000])
-
-try:
-    with open('trend.html', 'w', encoding='utf-8') as file:
-        file.write(response.text)
-        print("File written successfully.")
-except Exception as e:
-    print("An error occurred while writing to the file:", str(e))
-
-doc = BeautifulSoup(response.text, 'html.parser')
-print('Page title:', doc.title.text)
-
-# Container name: style-scope ytd-video-renderer
-
-video_divs = doc.find_all('div',
-class_='style-scope ytd-video-renderer')
-
-print(f'Found {len(video_divs)} videos')
+SENDER_PASSWORD = os.environ['GMAIL_PASSWORD']
+print(SENDER_PASSWORD)
--- scratch.py
+++ scratch.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import smtplib
+import os
 trending_url = 'https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl'
 
 driver = webdriver.Chrome()
 driver.get(trending_url)
-print('---------------------------------------------------')
+
 print('Page title:', driver.title) #page title
 
 container_name = "ytd-video-renderer" #name html class
@@ -32,7 +33,6 @@ def get_thumbnail(i):
     print(f'URL of thumbnail: {thumb}')
 
 def get_channel_name(i):
-    print("-----------------------------------Channel Name------------------------")
     # Channel name
     channel_name = container_elements[i].find_element(By.CSS_SELECTOR, 'a.yt-simple-endpoint.style-scope.yt-formatted-string')
     chan_name = channel_name.text 
@@ -41,37 +41,72 @@ def get_channel_name(i):
 
 
 def get_views(i):
-    print('--------------------------Views Number-----------------------')
     views = container_elements[i].find_element(By.CSS_SELECTOR, 'span.inline-metadata-item.style-scope.ytd-video-meta-block:nth-of-type(1)')
     views_no = views.text
     print(f"View Counting: {views_no}")
-    print('---------------------------------------------------------------')
+
 
 def get_upload(i):
-    print('--------------------------UPLOAD DURATION----------------------------')
     # Extracting Duration
     time_element = container_elements[i].find_element(By.CSS_SELECTOR, 'span.inline-metadata-item.style-scope.ytd-video-meta-block:nth-of-type(2)')
     time_text = time_element.text
     print(f'Upload duration: {time_text}')
-    print('----------------------')
 
 
-def get_duration(i):
-    print("-------------------Description---------------------------------")
+def get_description(i):
     description_text = container_elements[i].find_element(By.ID, 'description-text').text
     print(description_text)
 
 
 def main(i):
-    print("============================================================")
     get_name(i)
     get_link(i)
     get_thumbnail(i)
     get_channel_name(i)
     get_views(i)
     get_upload(i)
-    get_duration(i)
+    get_description(i)
     print("============================================================")
 
 for i in range(len(container_elements)):
     main(i)
+
+
+#######################################3
+
+# print("---------Sending result to Email---------------")
+
+# def send_email():
+#     # Email credentials
+#     SENDER_EMAIL = 'olinabin801@gmail.com'
+#     RECEIVER_EMAILS = ['nabinoli2004@gmail.com', 'olinabin2004@gmail.com']
+#     SENDER_PASSWORD = os.environ.get('GMAIL_PASSWORD')
+
+#     if SENDER_PASSWORD:
+#         try:
+#             server = smtplib.SMTP('smtp.gmail.com', 587)
+#             server.ehlo()
+#             server.starttls()
+#             server.login(SENDER_EMAIL, SENDER_PASSWORD)
+
+#             subject = 'Hello Nabin, How are you?'
+#             body = 'Hi MATE!'
+
+#             email_text = f"""\
+#             From: {SENDER_EMAIL}
+#             To: {', '.join(RECEIVER_EMAILS)}
+#             Subject: {subject}
+
+#             {body}
+#             """
+
+#             server.sendmail(SENDER_EMAIL, RECEIVER_EMAILS, email_text)
+#             server.close()
+#             print("Email sent successfully!")
+#         except Exception as e:
+#             print("An error occurred while sending the email:", str(e))
+#     else:
+#         print("Email not sent. Please ensure you have set the GMAIL_PASSWORD environment variable.")
+
+
+# send_email()

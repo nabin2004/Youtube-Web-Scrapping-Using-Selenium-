2ec5470d58b81ade83e4dee52b036753bd655dd6
nabin2004 <olinabin2004@gmail.com>
2023-06-26 10:36:54 +0545

    Made for total videos in Trending page

--- scratch.py
+++ scratch.py
@@ -20,43 +20,58 @@ def get_name(i):
     print('Title:', title)
 
 def get_link(i):
+    video = container_elements[i].find_element(By.ID, 'video-title')
     #link of video
     href = video.get_attribute('href')
     print(f'URL of video: {href}')
 
-
 #Thumbnail link of video
-thumbnail_tag = container_elements[0].find_element(By.TAG_NAME, 'img')
-thumb = thumbnail_tag.get_attribute('src')
-print(f'URL of thumbnail: {thumb}')
+def get_thumbnail(i):
+    thumbnail_tag = container_elements[i].find_element(By.TAG_NAME, 'img')
+    thumb = thumbnail_tag.get_attribute('src')
+    print(f'URL of thumbnail: {thumb}')
 
-print("-----------------------------------Channel Name------------------------")
-# Channel name
-channel_name = container_elements[0].find_element(By.CSS_SELECTOR, 'a.yt-simple-endpoint.style-scope.yt-formatted-string')
-chan_name = channel_name.text 
-print(f"Channel Name: {chan_name}")
+def get_channel_name(i):
+    print("-----------------------------------Channel Name------------------------")
+    # Channel name
+    channel_name = container_elements[i].find_element(By.CSS_SELECTOR, 'a.yt-simple-endpoint.style-scope.yt-formatted-string')
+    chan_name = channel_name.text 
+    print(f"Channel Name: {chan_name}")
 
 
-print('--------------------------Views Number-----------------------')
-# Views Number
-# views = container_elements[0].find_element(By.CSS_SELECTOR, 'div.span.inline-metadata-item style-scope ytd-video-meta-block')
-views = container_elements[0].find_element(By.CSS_SELECTOR, 'span.inline-metadata-item.style-scope.ytd-video-meta-block:nth-of-type(1)')
-views_no = views.text
-print(f"View Counting: {views_no}")
-print('---------------------------------------------------------------')
 
-print('--------------------------UPLOAD DURATION----------------------------')
-# Extracting Duration
-time_element = container_elements[0].find_element(By.CSS_SELECTOR, 'span.inline-metadata-item.style-scope.ytd-video-meta-block:nth-of-type(2)')
-time_text = time_element.text
-print(f'Upload duration: {time_text}')
-print('----------------------')
-print("-------------------Description---------------------------------")
-description_text = container_elements[0].find_element(By.ID, 'description-text').text
-print(description_text)
+def get_views(i):
+    print('--------------------------Views Number-----------------------')
+    views = container_elements[i].find_element(By.CSS_SELECTOR, 'span.inline-metadata-item.style-scope.ytd-video-meta-block:nth-of-type(1)')
+    views_no = views.text
+    print(f"View Counting: {views_no}")
+    print('---------------------------------------------------------------')
 
-print('----------------------------------------------------')
-title_element = container_elements[0].find_element(By.ID, 'video-title')
-title = title_element.get_attribute('title')
-print('Title:', title)
+def get_upload(i):
+    print('--------------------------UPLOAD DURATION----------------------------')
+    # Extracting Duration
+    time_element = container_elements[i].find_element(By.CSS_SELECTOR, 'span.inline-metadata-item.style-scope.ytd-video-meta-block:nth-of-type(2)')
+    time_text = time_element.text
+    print(f'Upload duration: {time_text}')
+    print('----------------------')
 
+
+def get_duration(i):
+    print("-------------------Description---------------------------------")
+    description_text = container_elements[i].find_element(By.ID, 'description-text').text
+    print(description_text)
+
+
+def main(i):
+    print("============================================================")
+    get_name(i)
+    get_link(i)
+    get_thumbnail(i)
+    get_channel_name(i)
+    get_views(i)
+    get_upload(i)
+    get_duration(i)
+    print("============================================================")
+
+for i in range(len(container_elements)):
+    main(i)
